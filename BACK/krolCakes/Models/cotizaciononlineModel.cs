c416ca94be357da2c04d1249eaec328139f67cc6
joseandresespa <jespanaj@miumg.edu.gt>
2024-09-09 20:35:13 -0600

    actualizacion de modelo de cotizacion

--- BACK/krolCakes/Models/cotizaciononlineModel.cs
+++ BACK/krolCakes/Models/cotizaciononlineModel.cs
@@ -3,12 +3,13 @@
     public class cotizaciononlineModel
     {
         public int? id { get; set; }
+        public string? nombre { get; set; }
         public string? descripcion { get; set; }
+        public string? direccion { get; set; }
         public int? telefono { get; set; }
-        public int? porciones { get; set; }
-        public int? cant_cupcakes { get; set; }
-        public double? precio_aproximado { get; set; }
-        public bool? envio { get; set; }
-        public double? total { get; set; }
+        public DateOnly? fecha { get; set; }
+        public TimeOnly? hora { get; set; }
+        public List<imagenreferenciaonlineModel>? imagenes { get; set; }
+        public List<productoModel>? productos { get; set; }
     }
 }
