b8d447535cc528bdb092fa4edcce08f00884126e
joseandresespa <jespanaj@miumg.edu.gt>
2024-09-17 19:05:24 -0600

    agregue campos a pedido de presupuesto

--- BACK/krolCakes/Models/pedidoModel.cs
+++ BACK/krolCakes/Models/pedidoModel.cs
@@ -11,6 +11,8 @@
         public string? direccion { get; set; }
         public int? id_tipo_entrega { get; set; }
         public double? precio_total { get; set; }
+        public double? mano_obra { get; set; }
+        public double? presupuesto_insumos { get; set; }
     }
     public class pedidoModelCompleto
     {
@@ -23,6 +25,8 @@
         public string? direccion { get; set; }      //proviene de modelo pedido
         public int? id_tipo_entrega { get; set; }   //proviene de modelo pedido
         public double? precio_total { get; set; }   //proviene de modelo pedido
+        public double? mano_obra { get; set; }
+        public double? presupuesto_insumos { get; set; }
         public string? estado { get; set; }         //proviene de modelo estado      
         public string? nombre { get; set; }        //nombre de cliente 
         public int? telefono { get; set; }        //proviene de modelo cliente   
